4e045f99c6af3365207ed099fc6a79863f257a1b
gowtham raj <gowtham4466@gmail.com>
2018-06-03 22:34:53 +0530

    a modified version of our game

--- js/tic.js
+++ js/tic.js
@@ -1,63 +1,16 @@
 var val = "O";
-
+var animationimages=['./images/1.png','./images/2.png','./images/3.png','./images/4.png','./images/5.png','./images/6.png','./images/11.png','./images/8.png','./images/9.png','./images/3.png','./images/6.png'];
  var movements=[]; 
-//setzt Buttons zurück
-// function zuruecksetMovements() {
-//     var b1 = document.getElementById("1");
-//     var b2 = document.getElementById("2");
-//     var b3 = document.getElementById("3");
-//     var b4 = document.getElementById("4");
-//     var b5 = document.getElementById("5");
-//     var b6 = document.getElementById("6");
-//     var b7 = document.getElementById("7");
-//     var b8 = document.getElementById("8");
-//     var b9 = document.getElementById("9");
-    
-//     b1.value="";
-//     b2.value="";
-//     b3.value="";
-//     b4.value="";
-//     b5.value="";
-//     b6.value="";
-//     b7.value="";
-//     b8.value="";
-//     b9.value="";
-//     //b1.disabled=false;
-//     b2.disabled=false;
-//     b3.disabled=false;
-//     b4.disabled=false;
-//     b5.disabled=false;
-//     b6.disabled=false;
-//     b7.disabled=false;
-//     b8.disabled=false;
-//     b9.disabled=false;
-    
-//     document.getElementById("popup").style.visibility = "hidden";
-//     document.getElementById("overlay").style.visibility = "hidden";
-// }
-
-// function buttonsdeaktivieren() {
-//     var b1 = document.getElementById("1");
-//     var b2 = document.getElementById("2");
-//     var b3 = document.getElementById("3");
-//     var b4 = document.getElementById("4");
-//     var b5 = document.getElementById("5");
-//     var b6 = document.getElementById("6");
-//     var b7 = document.getElementById("7");
-//     var b8 = document.getElementById("8");
-//     var b9 = document.getElementById("9");
-    
-//     //b1.disabled = true;
-//     b2.disabled = true;
-//     b3.disabled = true;
-//     b4.disabled = true;
-//     b5.disabled = true;
-//     b6.disabled = true;
-//     b7.disabled = true;
-//     b8.disabled = true;
-//     b9.disabled = true;
-// }
-
+ var childs=[];
+ var childadding;
+ for(var i=0;i<animationimages.length;i++){
+    childadding=document.createElement("img");
+    childadding.setAttribute("src",animationimages[i]);
+    childadding.setAttribute("width","100px");
+    childadding.setAttribute("height","100px");
+    childadding.setAttribute("id","flow");
+    childs.push(childadding);
+ }
 function cleardiv() {
      for(var iter=1;iter<=9;iter++){
          document.getElementById(iter).style.pointerEvents='none';
@@ -65,9 +18,9 @@ function cleardiv() {
      }
 }
 
-//prüft, ob Spiel zu Ende ist
+
 function compare() {
-    //Zustand der Buttons wird ausgelesen
+    //getting every div id
     var b1 = document.getElementById("1").value;
     var b2 = document.getElementById("2").value;
     var b3 = document.getElementById("3").value;
@@ -80,7 +33,7 @@ function compare() {
 
     var ans;
     var points=[];
-    //obere Reihe
+    //checking with possible positions
     if (((b1=="X") || (b1=="O")) && ((b1 == b2) && (b2 == b3))) {
         ans=b1;
         points.push("1");
@@ -92,7 +45,6 @@ function compare() {
         
 
     }
-    //linke Spalte
     else if (((b1=="X") || (b1=="O")) && ((b1 == b4) && (b4 == b7))){
         ans=b1;
         points.push("1");
@@ -101,7 +53,6 @@ function compare() {
         cleardiv();
         movements=[];
     }
-    //untere Reihe
     else if (((b9=="X") || (b9=="O")) && ((b9 == b8) && (b8 == b7))){
         ans=b9;
         points.push("9");
@@ -110,7 +61,6 @@ function compare() {
         cleardiv();
         movements=[];
     }
-    //rechte Spalte
     else if (((b9=="X") || (b9=="O")) && ((b9 == b6) && (b6 == b3))){
         ans=b9;
         points.push("9");
@@ -119,7 +69,6 @@ function compare() {
       cleardiv();
       movements=[];
     }
-    //mittlere Reihe
     else if (((b4=="X") || (b4=="O")) && ((b4 == b5) && (b5 == b6))){
         ans=b4;
         points.push("4");
@@ -128,7 +77,6 @@ function compare() {
       cleardiv();
       movements=[];
     }
-    //mittlere Spalte
     else if (((b2=="X") || (b2=="O")) && ((b2 == b5) && (b5 == b8))){
         ans=b2;
         points.push("2");
@@ -137,7 +85,6 @@ function compare() {
       cleardiv();
       movements=[];
     }
-    //1-9 Diagonale
     else if (((b1=="X") || (b1=="O")) && ((b1 == b5) && (b5== b9))){
         ans=b1;
         points.push("1");
@@ -146,7 +93,6 @@ function compare() {
       cleardiv();
       movements=[];
     }
-    //7-3 Diagonale
     else if (((b7=="X") || (b7=="O")) && ((b7 == b5) && (b5 == b3))){
         ans=b7;
         points.push("7");
@@ -156,28 +102,29 @@ function compare() {
       movements=[];
     }
     if(ans=="X"){
-
         for(var i=0;i<points.length;i++){
             document.getElementById(points[i]).innerHTML="<img src='images/red.png' class='animated bounceIn infinite' width=80px heigth=80px style='margin-top:18px;'>";
             document.getElementById("music").innerHTML="<embed src='winning.mp3' autostart='true' loop='true' width='2' height='0'></embed>"
-           
-        
         }
+       for(var i=0;i<childs.length;i++){
+        document.body.appendChild(childs[i]);
+       }
     }
-    else{
+    else if(ans=='O'){
         
         for(var i=0;i<points.length;i++){
-            document.getElementById(points[i]).innerHTML="<img src='images/heart.png' class='animated bounceIn infinite' width=80px heigth=80px style='margin-right:5px;'>";
+            document.getElementById(points[i]).innerHTML="<img src='images/heart.jpg' class='animated bounceIn infinite' width=80px heigth=80px style='margin-right:5px;'>";
             document.getElementById("music").innerHTML="<embed src='winning.mp3' autostart='true' loop='true' width='2' height='0'></embed>"
             
         }
+        for(var i=0;i<childs.length;i++){
+            document.body.appendChild(childs[i]);
+           }
     }
     }
 
   
-  
-  
-//X oder Y einsetMovements
+//fix the positions value
 function setMovements(x, val) {
     var button=null;
      if (x==1) {
@@ -192,56 +139,50 @@ function setMovements(x, val) {
      button.value = val;
      movements.push("2");
      document.getElementById('2').style.pointerEvents = 'none';
-    //  button.disabled=true;
+    
      }
      else if (x==3) {
       button = document.getElementById("3");
      button.value = val;
      movements.push("3");
      document.getElementById('3').style.pointerEvents = 'none';
-    //  button.disabled=true;
      }
      else if (x==4) {
       button = document.getElementById("4");
      button.value = val;
      movements.push("4");
      document.getElementById('4').style.pointerEvents = 'none';
-    //  button.disabled=true;
+
      }
      else if (x==5) {
       button = document.getElementById("5");
      button.value = val;
      movements.push("5");
      document.getElementById('5').style.pointerEvents = 'none';
-    //  button.disabled=true;
      }
      else if (x==6) {
       button = document.getElementById("6");
      button.value = val;
      movements.push("6");
      document.getElementById('6').style.pointerEvents = 'none';
-    //  button.disabled=true;
      }
      else if (x==7) {
       button = document.getElementById("7");
      button.value = val;
      movements.push("7");
      document.getElementById('7').style.pointerEvents = 'none';
-    //  button.disabled=true;
      }
      else if (x==8) {
       button = document.getElementById("8");
      button.value = val;
      movements.push("8");
      document.getElementById('8').style.pointerEvents = 'none';
-    //  button.disabled=true;
      }
      else if (x==9) {
       button = document.getElementById("9");
      button.value = val;
      movements.push("9");
      document.getElementById('9').style.pointerEvents = 'none';
-    //  button.disabled=true;
      }
      if(val=='X'){
         button.innerHTML = "<span class='glyphicon glyphicon-heart' style='color:red;font-size:60px;margin-top:20px;'></span>";
@@ -252,7 +193,7 @@ function setMovements(x, val) {
      compare();
      }
   
-//welches val?
+//start function
 function block(button) {
     if (val=="X") {
     val="O";
@@ -271,6 +212,9 @@ function resetAll(){
         document.getElementById(iter).innerHTML="";
     }
     document.getElementById("music").innerHTML="<embed src='beat.mp3' autostart='true' loop='true' width='2' height='0'></embed>"
+    for(var i=0;i<childs.length;i++){
+        $("#flow").remove();
+    }
     
 }
 function undo(){
